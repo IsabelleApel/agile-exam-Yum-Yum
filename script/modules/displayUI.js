1c472cfc2f9320b6aeadfde5069a3464df813314
Isabelle Apelgren <isabelleapelgren@gmail.com>
2025-03-07 19:07:51 +0100

    eventhandlers on buttons

--- script/modules/displayUI.js
+++ script/modules/displayUI.js
@@ -1,11 +1,17 @@
 import { randomNum, randomString } from "../Utils/utils.js";
 import { getElement } from "../Utils/domUtils.js";
+import { buttonClick } from "./eventHandlers.js";
 
 export function displayOrderConfirmation(){
     getETA();
     getOrderNum();
+    buttonClick('#newOrder', './menu.html');
+    // behöver veta namn på html-fil för kvittot(nedan)
+    buttonClick('#receipt', './receipt.html'); 
 }
 
+
+// vet inte riktigt vart det är passande att ha getETA() och getOrderNum()(vilken script-fil)
 function getETA(){
     let estimatedTime = randomNum(10, 20);
     let etaRef = getElement("#orderConfirmationETA");
