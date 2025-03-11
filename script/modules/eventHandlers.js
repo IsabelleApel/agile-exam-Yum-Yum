9565ce4ebf51a1a04b103b62864bdc9a986d09c4
Isabelle Apelgren <isabelleapelgren@gmail.com>
2025-03-11 18:16:23 +0100

    display header function ready

--- script/modules/eventHandlers.js
+++ script/modules/eventHandlers.js
@@ -7,3 +7,21 @@ export function buttonClick(element, pathname){
         window.location.href = pathname;
     })
 }
+
+export function menuToggle(){
+    const menuIconRef = getElement('.menu-icon');
+    const menuRef = getElement('.menu');
+
+    menuIconRef.addEventListener("click", function (e) {
+        e.stopPropagation(); 
+        menuRef.classList.toggle("open");
+    });
+
+    document.addEventListener("click", function (e) {
+        if (!menuRef.contains(e.target) && !menuIconRef.contains(e.target)) {
+          menuRef.classList.remove("open");
+        }
+      });
+}
+
+
--- script/script.js
+++ script/script.js
@@ -1,19 +1,22 @@
 
-import { displayMenu, displayOrderConfirmation, displayFoodtruckList } from "./modules/displayUI.js";
+import { displayOrderConfirmation, displayFoodtruckList, displayHeader } from "./modules/displayUI.js";
 
 if(window.location.pathname === '/' || window.location.pathname === '/index.html') {
     console.log('index.html');
 
-} else if(window.location.pathname === '/menu.html') {
-    console.log('menu.html');
-    displayMenu();
+// } else if(window.location.pathname === '/menu.html') {
+//     console.log('menu.html');
+//     displayMenu();
 
 } else if(window.location.pathname === '/orderConfirmation.html'){
     console.log('hello');
     displayOrderConfirmation();
+    displayHeader();
 }else if(window.location.pathname === '/foodtrucks.html'){
     console.log('foodtruck');
     displayFoodtruckList();
-    
+    displayHeader();
+}else if(window.location.pathname === '/aboutUs.html'){
+    displayHeader();
 }
 
