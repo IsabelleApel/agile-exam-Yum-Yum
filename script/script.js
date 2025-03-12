d956167cd00a960f9f6022a85645fce5bb300b71
Isabelle Apelgren <isabelleapelgren@gmail.com>
2025-03-12 09:14:15 +0100

    un-commented menu.html

--- script/script.js
+++ script/script.js
@@ -3,11 +3,10 @@ import { displayOrderConfirmation, displayFoodtruckList, displayHeader } from ".
 
 if(window.location.pathname === '/' || window.location.pathname === '/index.html') {
     console.log('index.html');
-
-// } else if(window.location.pathname === '/menu.html') {
-//     console.log('menu.html');
-//     displayMenu();
-
+} else if(window.location.pathname === '/menu.html') {
+    console.log('menu.html');
+    displayMenu();
+    displayHeader();
 } else if(window.location.pathname === '/orderConfirmation.html'){
     console.log('hello');
     displayOrderConfirmation();
