9f43ae66182ff216686f4c46a5aa2142488d3e53
Carolina-Cardenas <carolina.beatriz.cardenas@gmail.com>
2025-03-17 20:07:49 +0100

    created html for profile and styles

--- script/script.js
+++ script/script.js
@@ -1,27 +1,45 @@
-import { displayCart, displayTotalPrice, initCartCount } from "./modules/cart.js";
+import {
+  displayCart,
+  displayTotalPrice,
+  initCartCount,
+} from "./modules/cart.js";
 import { cartButton } from "./modules/eventHandlers.js";
-import { displayOrderConfirmation, displayFoodtruckList, displayHeader, displayMenu, displayCartPage, displayLandingPage, hideHeaderElement } from "./modules/displayUI.js";
+import {
+  displayOrderConfirmation,
+  displayFoodtruckList,
+  displayHeader,
+  displayMenu,
+  displayCartPage,
+  displayLandingPage,
+  hideHeaderElement,
+} from "./modules/displayUI.js";
 
-
-if(window.location.pathname === '/' || window.location.pathname === '/index.html') {
-    console.log('index.html');
-    displayLandingPage();
-} else if(window.location.pathname === '/menu.html') {
-    displayMenu();
-    displayHeader();
-} else if(window.location.pathname === '/orderConfirmation.html'){
-    displayOrderConfirmation();
-}else if(window.location.pathname === '/foodtrucks.html'){
-    displayFoodtruckList();
-    displayHeader();
-} else if(window.location.pathname === '/cart.html') {
-    displayCartPage();
-} else if(window.location.pathname === '/aboutUs.html'){
-    displayHeader();
-} else if(window.location.pathname === '/receipt.html'){
-    // displayReceipt();
-    displayHeader().then(() => {
-        hideHeaderElement('.menu-icon');
-        hideHeaderElement('.header-shopping-bag');
-    })
+if (
+  window.location.pathname === "/" ||
+  window.location.pathname === "/index.html"
+) {
+  console.log("index.html");
+  displayLandingPage();
+} else if (window.location.pathname === "/menu.html") {
+  displayMenu();
+  displayHeader();
+} else if (window.location.pathname === "/orderConfirmation.html") {
+  displayOrderConfirmation();
+} else if (window.location.pathname === "/foodtrucks.html") {
+  displayFoodtruckList();
+  displayHeader();
+} else if (window.location.pathname === "/cart.html") {
+  displayCartPage();
+} else if (window.location.pathname === "/aboutUs.html") {
+  displayHeader();
+} else if (window.location.pathname === "/receipt.html") {
+  // displayReceipt();
+  displayHeader().then(() => {
+    hideHeaderElement(".menu-icon");
+    hideHeaderElement(".header-shopping-bag");
+  });
+} else if (window.location.pathname === "/profile.html") {
+  displayHeader().then(() => {
+    hideHeaderElement(".header-shopping-bag");
+  });
 }
